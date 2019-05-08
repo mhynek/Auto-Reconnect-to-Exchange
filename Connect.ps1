f863966b780d8ad6b241152c2230eea0c9398f1a
mhynek <36461543+mhynek@users.noreply.github.com>
2019-05-08 16:50:05 -0500

    Update Connect.ps1

--- Connect.ps1
+++ Connect.ps1
@@ -6,13 +6,13 @@ $credential= Get-StoredCredential -username adm@contoso.com
 Function Connect2OP {
     Get-PSSession -Name "OP" -ErrorAction SilentlyContinue |Remove-PSSession
     $Exchange = "mail.contoso.com"
-    $OPSession = New-PSSession -Name "OP" -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$Exchange/PowerShell/" -Authentication Kerberos
+     $OPSession = New-PSSession -Name "OP" -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$Exchange/PowerShell/" -Credential $credential -Authentication Kerberos
     Import-PSSession $OPSession -AllowClobber -prefix OnPrem
   }
 
 Function Connect2EXO {
     Get-PSSession -Name "EXO" -ErrorAction SilentlyContinue |Remove-PSSession
-    $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection -Name "EXO"
+    $EXOSession = New-PSSession -Name "EXO" -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection 
     Import-PSSession $EXOSession -AllowClobber -prefix 365
   }
 
